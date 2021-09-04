83c6807c5435acf13a6bb434921a56cdefb98737
DevilBoy007 <dylan_bakr@comcast.net>
2021-09-04 19:35:32 -0400

    added resume tab

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-var options = [false,false,false,false,false,false]
+var options = [false,false,false,false,false,false, false]
 var covers = [false,false, false, false, false]
-var checker = arr => arr.every(v => v === false)
+var checker = arr => arr.every(v => v === false) // this is awesome
 var contactInfo = {email: 'dylanbakr@gmail.com', text:'(815) 993-8931', mail:'425 W Hampton Drive'}
-function update(selected){
+function updateContact(selected){
   switch (selected)
   {
     case 'email':
       console.log('email')
-      document.getElementById("contact").href = `"mailto:${contactInfo.email}"`
-      document.getElementById("contact").innerHTML = contactInfo.email
+      document.getElementById('contact').href = `'mailto:${contactInfo.email}'`
+      document.getElementById('contact').innerHTML = contactInfo.email
       break;
     case 'text':
       console.log('text')
-      document.getElementById("contact").href = `"tel:${contactInfo.text}"`
-      document.getElementById("contact").innerHTML = contactInfo.text
+      document.getElementById('contact').href = `'tel:${contactInfo.text}'`
+      document.getElementById('contact').innerHTML = contactInfo.text
       break;
     case 'mail':
       console.log('mail')
-      document.getElementById("contact").href = "https://goo.gl/maps/bTkq7kuiwpWBuZMx6"
-      document.getElementById("contact").innerHTML = contactInfo.mail
+      document.getElementById('contact').href = 'https://goo.gl/maps/bTkq7kuiwpWBuZMx6'
+      document.getElementById('contact').innerHTML = contactInfo.mail
       break;
     default:
   }
@@ -26,113 +26,132 @@ function update(selected){
 
 function add(pane) {
   switch (pane) {
-    case "who":
-      var div = document.getElementById("whod")
-      var button = document.getElementById("who")
+    case 'who':
+      var div = document.getElementById('whod')
+      var button = document.getElementById('who')
       options[0]=!options[0]
       if(options[0])
       {
         button.classList.add('button-selected')
-        div.style.display = "block"
+        div.style.display = 'block'
         div.scrollIntoView('smooth')
       }
       if(!options[0])
       {
         button.classList.remove('button-selected')
-        div.style.display = "none"
+        div.style.display = 'none'
       }
-      if(checker(options)){document.getElementById("top").style.visibility="hidden"}
-      else(document.getElementById("top").style.visibility="visible")
+      if(checker(options)){document.getElementById('top').style.visibility='hidden'}
+      else(document.getElementById('top').style.visibility='visible')
       break;
-    case "what":
-      var div = document.getElementById("whatd")
-      var button = document.getElementById("what")
+    case 'what':
+      var div = document.getElementById('whatd')
+      var button = document.getElementById('what')
       options[1]=!options[1]
       if(options[1])
       {
         button.classList.add('button-selected')
-        div.style.display = "block"
+        div.style.display = 'block'
         div.scrollIntoView('smooth')
       }
       if(!options[1])
       {
         button.classList.remove('button-selected')
-        div.style.display = "none"
+        div.style.display = 'none'
       }
-      if(checker(options)){document.getElementById("top").style.visibility="hidden"}
-      else(document.getElementById("top").style.visibility="visible")
+      if(checker(options)){document.getElementById('top').style.visibility='hidden'}
+      else(document.getElementById('top').style.visibility='visible')
       break;
-    case "where":
-      var div = document.getElementById("whered")
-      var button = document.getElementById("where")
+    case 'where':
+      var div = document.getElementById('whered')
+      var button = document.getElementById('where')
       options[2]=!options[2]
       if(options[2])
       {
         button.classList.add('button-selected')
-        div.style.display = "block"
+        div.style.display = 'block'
         div.scrollIntoView('smooth')
       }
       if(!options[2])
       {
         button.classList.remove('button-selected')
-        div.style.display = "none"
+        div.style.display = 'none'
       }
-      if(checker(options)){document.getElementById("top").style.visibility="hidden"}
-      else(document.getElementById("top").style.visibility="visible")
+      if(checker(options)){document.getElementById('top').style.visibility='hidden'}
+      else(document.getElementById('top').style.visibility='visible')
       break;
-    case "when":
-      var div = document.getElementById("whend")
-      var button = document.getElementById("when")
+    case 'when':
+      var div = document.getElementById('whend')
+      var button = document.getElementById('when')
       options[3]=!options[3]
       if(options[3])
       {
         button.classList.add('button-selected')
-        div.style.display = "block"
+        div.style.display = 'block'
         div.scrollIntoView('smooth')
       }
       if(!options[3])
       {
         button.classList.remove('button-selected')
-        div.style.display = "none"
+        div.style.display = 'none'
       }
-      if(checker(options)){document.getElementById("top").style.visibility="hidden"}
-      else(document.getElementById("top").style.visibility="visible")
+      if(checker(options)){document.getElementById('top').style.visibility='hidden'}
+      else(document.getElementById('top').style.visibility='visible')
       break;
-    case "why":
-      var div = document.getElementById("whyd")
-      var button = document.getElementById("why")
+    case 'why':
+      var div = document.getElementById('whyd')
+      var button = document.getElementById('why')
       options[4]=!options[4]
       if(options[4])
       {
         button.classList.add('button-selected')
-        div.style.display = "block"
+        div.style.display = 'block'
         div.scrollIntoView('smooth')
       }
       if(!options[4])
       {
         button.classList.remove('button-selected')
-        div.style.display = "none"
+        div.style.display = 'none'
       }
-      if(checker(options)){document.getElementById("top").style.visibility="hidden"}
-      else(document.getElementById("top").style.visibility="visible")
+      if(checker(options)){document.getElementById('top').style.visibility='hidden'}
+      else(document.getElementById('top').style.visibility='visible')
       break;
-    case "how":
-      var div = document.getElementById("howd")
-      var button = document.getElementById("how")
+    case 'how':
+      var div = document.getElementById('howd')
+      var button = document.getElementById('how')
       options[5]=!options[5]
       if(options[5])
       {
         button.classList.add('button-selected')
-        div.style.display = "block"
+        div.style.display = 'block'
         div.scrollIntoView('smooth')
       }
       if(!options[5])
       {
         button.classList.remove('button-selected')
-        div.style.display = "none"
+        div.style.display = 'none'
       }
-      if(checker(options)){document.getElementById("top").style.visibility="hidden"}
-      else(document.getElementById("top").style.visibility="visible")
+      if(checker(options)){document.getElementById('top').style.visibility='hidden'}
+      else{document.getElementById('top').style.visibility='visible'}
+      break;
+    case 'resume':
+      var div = document.getElementById('resumed')
+      var button = document.getElementById('resume')
+      var doc = document.getElementById('pdf')
+      options[6]=!options[6]
+      if(options[6])
+      {
+        button.classList.add('button-selected')
+        div.style.visibility='visible'
+        div.scrollIntoView('smooth')
+      }
+      if(!options[6]) 
+      { 
+        div.style.visibility = 'hidden'
+        button.classList.remove('button-selected')
+      }
+      if (checker(options)) { document.getElementById('top').style.visibility = 'hidden' }
+      else { document.getElementById('top').style.visibility = 'visible' }
       break;
     default:
       break;
@@ -141,49 +160,44 @@ function add(pane) {
 function addInfo(cover){
   switch(cover)
   {
-    case "me":
-      var div = document.getElementById("me")
+    case 'me':
+      var div = document.getElementById('me')
       covers[0]=!covers[0]
       if(covers[0]){
-        div.style.display="block"
-        div.scrollIntoView("smooth")
+        div.style.display='block'
 
       }
-      else { document.getElementById("me").style.display = "none"}
+      else { document.getElementById('me').style.display = 'none'}
       break;
-    case "this":
+    case 'this':
       covers[1] = !covers[1]
       if (covers[1]) 
       {
-        document.getElementById("this").style.display = "block"
-        document.getElementById("this").scrollIntoView('smooth')
+        document.getElementById('this').style.display = 'block'
       }
-      else { document.getElementById("this").style.display="none"}
+      else { document.getElementById('this').style.display='none'}
       break;
-    case "game development":
+    case 'game development':
       covers[2]=!covers[2]
       if(covers[2])
       {
-        document.getElementById("gameDev").style.display="block"
-        document.getElementById("gameDev").scrollIntoView('smooth')
+        document.getElementById('gameDev').style.display='block'
       }
-      else {document.getElementById("gameDev").style.display="none"}
+      else {document.getElementById('gameDev').style.display='none'}
       break;
-    case "web development":
+    case 'web development':
       covers[3] = !covers[3]
       if (covers[3]) {
-        document.getElementById("webDev").style.display = "block"
-        document.getElementById("webDev").scrollIntoView('smooth')
+        document.getElementById('webDev').style.display = 'block'
       }
-      else { document.getElementById("webDev").style.display = "none" }
+      else { document.getElementById('webDev').style.display = 'none' }
       break;
-    case "robotics":
+    case 'robotics':
       covers[4] = !covers[4]
       if (covers[4]) {
-        document.getElementById("robotics").style.display = "block"
-        document.getElementById("robotics").scrollIntoView('smooth')
+        document.getElementById('robotics').style.display = 'block'
       }
-      else { document.getElementById("robotics").style.display = "none" }
+      else { document.getElementById('robotics').style.display = 'none' }
       break;
     default:
   }
