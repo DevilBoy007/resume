4c8c568f17a362303ecc2066fcf91c52b80be921
Dylan Bakr <dylan_bakr@comcast.net>
2022-03-13 05:01:52 -0400

    changed app.js, server.js, styles.css
    app.js
    ------
       • playing with scrollback to make tab open more naturally
    
    server.js
    ---------
       • updated active server status text
        • changed from static template to serve local index.html
    
    styles.css
    ----------
       • commented a stylesheet import for reference

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const express = require('express')
+const app = express()
+const port = 3000
 app.get('/', function (req,res) {
-	res.sendFile(__dirname + '/templates/landing_page.html');
-});
-app.use(express.static(__dirname + '/'));
-app.listen(port, () => console.log('server is running'));
+	res.sendFile(__dirname + '/index.html')
+	console.log('you have a visitor!')
+})
+app.use(express.static(__dirname + '/'))
+app.listen(port, () => console.log('server is running...'))
